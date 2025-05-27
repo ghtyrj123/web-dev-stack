583b82535b7f8321f41c6a463ad4bb5191c78a89
ghtyrj123 <ghtyrj1234@gmail.com>
2025-05-27 17:48:26 +0900

    05-27

---
+++ 03_JavaScript/자바실습/자바실습6.js
@@ -0,0 +1,7 @@
+const time = document.querySelector("#time");
+const now = new Date();
+function time() {}
+console.log(
+  `${now.getFullYear()}년 ${now.getMonth() + 1}월 ${now.getDate()}일`
+);
+console.log(now);
