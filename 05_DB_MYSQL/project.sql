52c38d11b5f5c03b76a32a8d91a4b2a80501ac40
ghtyrj123 <ghtyrj1234@gmail.com>
2025-06-12 17:49:04 +0900

    06-12

---
+++ 05_DB_MYSQL/project.sql
@@ -0,0 +1,15 @@
+-- 채무관리
+
+-- 거래처이름
+
+-- 순이익률
+-- 합계
+CREATE TABLE DEBT_MANAGEMRNT(
+DEBT_MANAGEM_NO INT AUTO_INCREMENT PRIMARY KEY,
+TRANSATION_NAME VARCHAR(50), -- 거래처이름
+PROFIT  FLOAT,		-- 이익률
+CHERGE  FLOAT,		-- 수수료
+
+
+);ㅇㅇ
+블랙잭  시퀀스1 리맠 오리진 제네무적+템오카 블랙잭 조커+라모르  템오카 얼드 리맠 
